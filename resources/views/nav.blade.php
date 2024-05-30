1ee04d3108b703bc53b18ed8cd931f2afe9054b1
Sobit Karki <sobitkarki7@gmail.com>
2024-05-30 09:06:08 +0545

    added entry page

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</head>
-<body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Navbar</a>
@@ -19,6 +8,12 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="first">First Page</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="second">Second Page</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
@@ -45,5 +40,3 @@
           </div>
         </div>
       </nav>
-</body>
-</html>
--- resources/views/second.blade.php
+++ resources/views/second.blade.php
@@ -1,4 +1,6 @@
 @extends('layout')
 @section('title', 'Second Page')
 @section('content')
+ <h1>Second Page</h1>
+ <p>This this the contents of the second page</p>
 @endsection
